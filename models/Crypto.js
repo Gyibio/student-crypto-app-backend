13ff82788ebc4eff4eb28bac07e8b91cb7b08fd0
Nana Kwame Adom-Boateng <royadom480@gamil.com>
2026-04-29 14:44:46 +0000

    Define models

---
+++ models/Crypto.js
@@ -0,0 +1,30 @@
+const { default: mongoose, mongo } = require("mongoose");
+
+const CryptoSchema =  mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+    },
+    Symbol: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    change24h: {
+        type: Number,
+        required: true,
+    }, //percentage change
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+module.exports = mongoose.model('Crypto', CryptoSchema);
