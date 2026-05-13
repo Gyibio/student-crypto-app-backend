f0b562264f66cdfb5a2582fc906e5360ed649c8d
Nana Kwame Adom-Boateng <royadom480@gamil.com>
2026-05-13 18:21:10 +0000

    update profile route

--- routes/auth.js
+++ routes/auth.js
@@ -62,7 +62,7 @@ router.post("/login", async (req, res) => {
 //Profile (Get)
 router.get("/profile", authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(verified._id).select("-password");
+    const user = await User.findById(req.user._id).select("-password");
     if (!user) return res.status(404).send("User not found");
 
     res.json(user);
